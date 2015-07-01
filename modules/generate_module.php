b66498c70b2ee1ed83c22ff1646848aae549213b
高見知英 <chie@onpu-tamago.net>
2015-07-01 22:43:40 +0900

    Squashed 'generate/' content from commit 09a726e
    git-subtree-dir: generate
    git-subtree-split: 09a726ef197e9960b2fe0b1cc2cd0802034ce729

---
+++ modules/generate_module.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Example Generate Modules
+ *
+ * @author TakamiChie
+ * @link http://onpu-tamago.net/
+ * @license http://opensource.org/licenses/MIT
+ * @version 1.0
+ */
+class Generate_Module
+{
+  public function run($settings)
+  {
+  }
+}
+?>
---
+++ page_generator.bat
@@ -0,0 +1,2 @@
+@echo off
+php page_generator.php
