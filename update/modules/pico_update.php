4f9bd3281659bdbee955b0189d724793738ce4c6
高見知英 <chie@onpu-tamago.net>
2015-11-20 16:13:27 +0900

    コード例を追加

---
+++ update/modules/pico_update.php
@@ -0,0 +1,14 @@
+<?php
+
+class Pico_Update{
+
+  public function precheck(){
+    return "";
+  }
+
+  public function run(){
+    return array("success" => true, "message" => "No Message");
+  }
+}
+
+?>
