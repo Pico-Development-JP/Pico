33687c74b21887ddd02f01647f1866426b877953
高見知英 <chie@onpu-tamago.net>
2016-02-24 20:50:34 +0900

    1.0に対応(ジェネレータクラスをPicoクラスを継承したクラスに変更)
    あわせて不要になったPico引用関数を全て削除

--- generate/page_generator.php
+++ generate/page_generator.php
@@ -2,26 +2,21 @@
 define('ROOT_DIR', realpath(__DIR__ . '/../') . '/');
 define('GENERATOR_DIR', realpath(__DIR__ . '/modules/') . '/');
 define('LOG_DIR', realpath(__DIR__ . '/logs/') . '/');
+require_once(ROOT_DIR . "lib/Pico.php");
 
 if(count($argv) > 1){
   $names = array_slice($argv, 1);
 }else{
   $names = false;
 }
-$gen = new Page_Generator();
-$gen->run($names);
 
-class Page_Generator{
+class Page_Generator extends Pico {
 
-  private $config;
-
-  public function __construct(){
-    $this->config = $this->get_config();
-  }
-
-  public function run($modulenames){
-    $modules = $this->get_files(GENERATOR_DIR, '.php');  
-    $args = array($this->config);
+  public function run($modulenames = null){
+    $modules = $this->getFiles(GENERATOR_DIR, '.php');
+    $this->loadConfig();
+    
+    $args = array($this->getConfig());
     // Copy from pico.php(load_plugins(), run_hooks())
     if (!empty($modules)) {
       foreach ($modules as $module) {
@@ -44,67 +39,13 @@ class Page_Generator{
     }
   }
 
-  // Copy from pico.php
-  
-  /**
-   * Loads the config
-   *
-   * @return array $config an array of config values
-   */
-  protected function get_config()
-  {
-
-    $this->config = @include_once(ROOT_DIR . 'config.php');
-
-    $defaults = array(
-      'theme' => 'default',
-      'date_format' => '%D %T',
-      'twig_config' => array('cache' => false, 'autoescape' => false, 'debug' => false),
-      'pages_order_by' => 'alpha',
-      'pages_order' => 'asc',
-      'excerpt_length' => 50,
-      'content_dir' => 'content-sample/',
-    );
-
-    if (is_array($this->config)) {
-      $this->config = array_merge($defaults, $this->config);
-    } else {
-      $this->config = $defaults;
-    }
-
-    return $this->config;
-  }
-
-  /**
-   * Helper function to recusively get all files in a directory
-   *
-   * @param string $directory start directory
-   * @param string $ext optional limit to file extensions
-   * @return array the matched files
-   */
-  protected function get_files($directory, $ext = '')
-  {
-    $array_items = array();
-    if ($handle = opendir($directory)) {
-      while (false !== ($file = readdir($handle))) {
-        if (in_array(substr($file, -1), array('~', '#'))) {
-            continue;
-        }
-        if (preg_match("/^(^\.)/", $file) === 0) {
-          if (is_dir($directory . "/" . $file)) {
-            $array_items = array_merge($array_items, $this->get_files($directory . "/" . $file, $ext));
-          } else {
-            $file = $directory . "/" . $file;
-            if (!$ext || strstr($file, $ext)) {
-                $array_items[] = preg_replace("/\/\//si", "/", $file);
-            }
-          }
-        }
-      }
-      closedir($handle);
-    }
-
-    return $array_items;
-  }
-
 }
+
+// instance Pico
+$gen = new Page_Generator(
+    ROOT_DIR,    // root dir
+    ROOT_DIR . 'config/',  // config dir
+    ROOT_DIR . 'plugins/', // plugins dir
+    ROOT_DIR . 'themes/'   // themes dir
+);
+$gen->run($names);
