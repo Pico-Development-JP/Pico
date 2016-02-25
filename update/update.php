8fc967a31d4ac5f1b357933ea579a00aeaf81bc3
高見知英 <chie@onpu-tamago.net>
2016-02-25 11:05:07 +0900

    1.0対応

--- update/update.php
+++ update/update.php
@@ -1,33 +1,33 @@
 <?php
 require_once "update_util.php";
 
-$update = new Update();
-$update->precheck();
-
-class Update{
+class Update extends Update_Util{
 
   public function precheck(){
     chdir(ROOT_DIR);
-    $util = Update_Util::getInstance();
+    $this->loadConfig();
+
     $success = FALSE;
     $msg = "Module Not Found";
-    $args = array($util->config);
+    $args = array($this->getConfig());
     $p = $_SERVER['QUERY_STRING'];
     $found = FALSE;
     if($p){
-      $modules = $util->get_files(UPDATER_DIR, '.php', 1);
+      $modules = $this->getFiles(UPDATER_DIR, '.php');
       parse_str($p, $request);
       $module = "";
       // 検索
       foreach ($modules as $module) {
-        include_once($module);
-        $module_name = preg_replace("/\\.[^.\\s]{3}$/", '', basename($module));
-        if (class_exists($module_name) && ($module_name == $request["name"])) {
-          $obj = new $module_name;
-          if (is_callable(array($obj, "precheck")) &&
-            is_callable(array($obj, "run"))) {
-            $msg = call_user_func_array(array($obj, "precheck"), $args);
-            $found = $module; 
+        if(strpos($module, "vendor") === false){
+          include_once($module);
+          $module_name = preg_replace("/\\.[^.\\s]{3}$/", '', basename($module));
+          if (class_exists($module_name) && ($module_name == $request["name"])) {
+            $obj = new $module_name;
+            if (is_callable(array($obj, "precheck")) &&
+              is_callable(array($obj, "run"))) {
+              $msg = call_user_func_array(array($obj, "precheck"), $args);
+              $found = $module; 
+            }
           }
         }
       }
@@ -44,7 +44,7 @@ class Update{
         }
 
         if($msg){
-          $util->sendWebhook($msg, "Update Accepted");
+          $this->sendWebhook($msg, "Update Accepted");
           echo "$msg";
         }else{
           echo "<h1>Update Accepted</h1>\n";
@@ -52,10 +52,19 @@ class Update{
       }else{
         $msg = "Module Not Found";
         echo "<h1>$msg</h1>\n";
-        $util->sendWebhook($msg, "Update Failed");
+        $this->sendWebhook($msg, "Update Failed");
       }
       return $msg;
     }
   }
 
 }
+
+// instance Pico
+$update = new Update(
+    ROOT_DIR,    // root dir
+    ROOT_DIR . 'config/',  // config dir
+    ROOT_DIR . 'plugins/', // plugins dir
+    ROOT_DIR . 'themes/'   // themes dir
+);
+$update->precheck();
